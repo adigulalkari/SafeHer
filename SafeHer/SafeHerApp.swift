db4b96883b3d76cdf795ff06bb1a903ddc2d0fb5
adigulalkari <adigulalkari825@gmail.com>
2024-09-11 16:50:15 +0530

    Initial Commit

---
+++ SafeHer/SafeHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeHerApp.swift
+//  SafeHer
+//
+//  Created by Adi Gulalkari on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
